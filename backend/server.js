04c5fb4fdc11fe5a86c53051345eac4f7403260e
Alan Jack <60137518+aldojack@users.noreply.github.com>
2022-11-08 14:55:52 +0000

    connected DB to the backend

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,34 @@
+require('dotenv').config();
 const express = require('express');
+const {MongoClient} = require('mongodb');
+const uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PWD}@cluster0.cuw5ckt.mongodb.net/?retryWrites=true&w=majority`
 const app = express();
-const port = 3000;
+const port = 8000;
 
 app.get('/',(req,res) => {
     res.send("Hello from express");
 })
 
-app.get('/login',(req,res) => {
+app.post('/signup',(req,res) => {
+    //Still a work in process - need to create a form on  the front end to send data here then validate before sending the mongoDB
     res.send("Hello from login");
+})
+
+app.get('/account',async (req,res) => {
+    //Once signed in then display users details
+    const client = new MongoClient(uri);
+
+    try{
+        await client.connect();
+        const db = client.db('FDM');
+        const users = db.collection('Users');
+
+        const returnedUsers =  await users.find().toArray();
+        res.send(returnedUsers);
+    }
+    finally{
+        await client.close();
+    }
 })
 
 app.listen(port, () => {
