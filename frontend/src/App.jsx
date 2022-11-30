4b12fdb7d97813410226f89d2c8937ad4fbae10e
Alan Jack <60137518+aldojack@users.noreply.github.com>
2022-11-30 17:33:42 +0000

    Imported react-router and implemented into app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,18 @@
-import Header from "./components/Header";
-import Nav from "./components/Nav";
-import Body from "./components/Body";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import OnBoarding from "./pages/Onboarding";
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+
 
 export default function App() {
   return (
-    <div>
-      <Nav/>
-      <Header/>
-      <Body/>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/dashboard" element={<Dashboard/>}/>
+      <Route path="/onboarding" element={<OnBoarding/>}/>
+    </Routes>
+  </BrowserRouter>
   )
 }
