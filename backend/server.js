9c1b91ec3e043fc04b10b576520800a28fef88fb
Alan Jack <60137518+aldojack@users.noreply.github.com>
2022-12-05 22:40:43 +0000

    uuid, jwt, bcrypt and  cors installed with post register route

--- backend/server.js
+++ backend/server.js
@@ -2,26 +2,71 @@ require('dotenv').config();
 const express = require('express');
 const {MongoClient} = require('mongodb');
 const uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PWD}@cluster0.cuw5ckt.mongodb.net/?retryWrites=true&w=majority`
+const { v4: uuidv4 } = require('uuid');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const cors = require('cors');
 const app = express();
 const port = 8000;
+
+app.use(cors());
+app.use(express.json())
 
 app.get('/',(req,res) => {
     res.send("Hello from express");
 })
 
-app.post('/signup',(req,res) => {
-    //Still a work in process - need to create a form on  the front end to send data here then validate before sending the mongoDB
+app.post('/register',async (req,res) => {
+    const client = new MongoClient(uri);
+    const {firstName, lastName, email, password} = req.body;
+
+
+
+    try{
+        await client.connect();
+        const db = client.db('FDM');
+        const users = db.collection('Users');
+
+        const existingUser = await users.findOne({email});
+        if(existingUser){
+            return res.status(409).send('Email already exists, please login');
+        }
+        const generateUserId = uuidv4();
+        const sanitizedFirstName = firstName.toLowerCase();
+        const sanitizedLastName = lastName.toLowerCase();
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const sanitizedEmail = email.toLowerCase();
+        
+        const data = {
+            user_id: generateUserId, 
+            firstName: sanitizedFirstName,
+            lastName: sanitizedLastName,
+            email: sanitizedEmail, 
+            password: hashedPassword
+        }
+        const newUser = await users.insertOne(data);
+        //Sign up for secret key
+        const token = jwt.sign(newUser, sanitizedEmail, {
+            expiresIn: 60 *24,
+        })
+        res.status(201).json({token, userId: generateUserId, email: sanitizedEmail})
+    } catch(err){
+        console.log(err);
+    }
+})
+
+app.post('/login',(req,res) => {
     res.send("Hello from login");
 })
 
-app.get('/account',async (req,res) => {
+app.get('/users',async (req,res) => {
     //Once signed in then display users details
     const client = new MongoClient(uri);
 
     try{
         await client.connect();
         const db = client.db('FDM');
-        const users = db.collection('users');
+        const users = db.collection('Users');
 
         const returnedUsers =  await users.find().toArray();
         res.send(returnedUsers);
