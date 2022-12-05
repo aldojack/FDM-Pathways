587ee1a9017ebfa16b7f53dd4db7106e5b3bc694
Alan Jack <60137518+aldojack@users.noreply.github.com>
2022-12-05 22:39:59 +0000

    AuthModal can now send post request to register to add new user

--- frontend/src/components/AuthModal.jsx
+++ frontend/src/components/AuthModal.jsx
@@ -1,23 +1,39 @@
 import { useState } from "react";
+import axios from 'axios';
+import {useNavigate} from 'react-router-dom'
 
 
 const AuthModal = ({setShowModal, isSignUp}) => {
 
+    const [firstName,  setFirstName] = useState(null);
+    const [lastName,  setLastName] = useState(null);
     const [email,  setEmail] = useState(null);
+    const [confirmEmail,  setConfirmEmail] = useState(null);
     const [password,  setPassword] = useState(null);
     const [confirmPassword,  setConfirmPassword] = useState(null);
     const [error,  setError] = useState(null);
 
+    let navigate = useNavigate();
+
     const handleClick = () => {
         setShowModal(false)
     }
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
+        // uncomment for client side validation of password/email
         e.preventDefault();
         try{
-            if(isSignUp && (password !== confirmPassword)){
-                setError("Passwords need to match!");
+            if(isSignUp && (password !== confirmPassword) || (email !== confirmEmail)){
+                setError("Please check your email and passwords match");
+                return
             }
-            console.log("Make a post request to the database");
+            
+            const response = await axios.post('http://localhost:8000/register', {firstName, lastName, email, password});
+            const success = response.status == 201
+            if(success) {
+                setShowModal(false)
+                navigate('/');
+            }
+
         } catch(error){
             console.log(error);
         }
@@ -27,15 +43,44 @@ const AuthModal = ({setShowModal, isSignUp}) => {
             <div className="close-icon float-right cursor-pointer" onClick={handleClick}>✖</div>
             <h2 className="text-lg font-bold">{isSignUp ? 'Create Account' : 'Login'}</h2>
             <p className="text-sm">By clicking login, you agree to our terms.  Learn how we process your data in our Privacy Policy and Cookie Policy</p>
-            <form className="flex flex-col" onSubmit={handleSubmit}>
+            <form className="flex flex-col" onSubmit={handleSubmit}>            
+            
+            {isSignUp && 
+            <>
                 <input 
+                    type="text"
+                    id="first_name"
+                    name="first_name"
+                    placeholder="enter first name"
+                    required={true}
+                    onChange={(e) => setFirstName(e.target.value)}
+                />
+
+                <input 
+                    type="text"
+                    id="last_name"
+                    name="last_name"
+                    placeholder="enter last name"
+                    required={true}
+                    onChange={(e) => setLastName(e.target.value)}
+                /> 
+            </>}
+            <input 
                     type="email"
                     id="email"
                     name="email"
                     placeholder="enter email"
                     required={true}
                     onChange={(e) => setEmail(e.target.value)}
-                />
+                />                
+                {isSignUp && <input 
+                    type="email"
+                    id="confirm_email"
+                    name="confirm_email"
+                    placeholder="confirm email"
+                    required={true}
+                    onChange={(e) => setConfirmEmail(e.target.value)}
+                />}
                 
                 <input 
                     type="password"
@@ -44,19 +89,19 @@ const AuthModal = ({setShowModal, isSignUp}) => {
                     placeholder="enter password"
                     required={true}
                     onChange={(e) => setPassword(e.target.value)}
-                />
+                />                
                 
                 {isSignUp && <input 
                     type="password"
-                    id="password-check"
-                    name="password-check"
+                    id="confirm_password"
+                    name="confirm_password"
                     placeholder="confirm password"
                     required={true}
                     onChange={(e) => setConfirmPassword(e.target.value)}
                 />}
 
-                <input className="outline-none bg-gradient-to-r from-blue-600 to-purple-600 rounded-3xl px-4 py-1 mt-6 hover:from-purple-600 hover:to-blue-600 text-white font-bold"type="submit"/>
-                <p>{error}</p>
+                <input className="outline-none bg-gradient-to-r from-blue-600 to-purple-600 rounded-3xl px-4 py-1 mt-6 hover:from-purple-600 hover:to-blue-600 text-white font-bold" type="submit" value={isSignUp ? 'Create Account' : 'Login'}/>
+                <p className="text-red-600">{error}</p>
             </form>
         </div>
     )
