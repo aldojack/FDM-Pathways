35872843a9baf4434d102a55c9591a0900328a40
Alan Jack <60137518+aldojack@users.noreply.github.com>
2022-12-11 12:28:07 +0000

    Home now gets user details that is signed in

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,14 +2,28 @@ import Nav from "../components/Nav";
 import Header from "../components/Header";
 import Body from "../components/Body";
 import AuthModal from "../components/AuthModal";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useCookies } from 'react-cookie';
+import axios from 'axios';
+
 
 
 export default function Home() {
     const [showModal, setShowModal] = useState(false);
     const [isSignUp, setIsSignUp] = useState(false);
     const [cookies,  setCookie, removeCookie] = useCookies(['user']);
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        if(AuthToken){
+            const getUser = async() => {
+                const userId = cookies.UserId;
+                const response = await axios.get('http://localhost:8000/user', userId);
+                setUser(response.data);
+            }
+            getUser();
+        }
+    },[])
   
     const AuthToken = cookies.AuthToken;
 
@@ -27,6 +41,7 @@ export default function Home() {
             cookies={cookies}
             removeCookie={removeCookie}
             AuthToken={AuthToken}
+            User={user}
         />
         <Header handleClick={handleClick} AuthToken={AuthToken}/>
         <div>                
