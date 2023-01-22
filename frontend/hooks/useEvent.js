463553f12a7dab8303bdd83e096ef850f5c79cf2
Alan Jack <60137518+aldojack@users.noreply.github.com>
2023-01-22 16:21:21 +0000

    Implement  timer and stop movement at end

--- frontend/hooks/useEvent.js
+++ frontend/hooks/useEvent.js
@@ -2,7 +2,16 @@ import { useEffect } from "react";
 
 export default function useEvent(event, handler, passive=false){
     useEffect(() => {
+        // passive = true
+        if(passive)
+        {
+            return
+        }
+        else {
+            window.addEventListener(event, handler, passive);
+        }
         window.addEventListener(event, handler, passive);
+        
     
         return function cleanUp(){
             window.removeEventListener(event, handler, passive);
--- frontend/src/components/Timer.jsx
+++ frontend/src/components/Timer.jsx
@@ -1,29 +1,20 @@
 import { useState, useEffect } from 'react';
 
-export default function Timer({isActive, setIsActive}) {
-    const [counter, setCounter] = useState(10);
+export default function Timer({isActive, setIsActive, counter, setCounter}) {
 
-
-    function toggle() {
-        setIsActive(!isActive);
-      }
 
       useEffect(() => {
-
+         //If active and time left then countdown timer will decrement by 1 second
          if(isActive && counter >0){
             setTimeout(() => {
                 setCounter(prevCounter => prevCounter - 1)
             }, 1000)
-         } else if(!isActive && counter <=0){
+         } 
+         //If counter is 0 then player is no longer active and movement should stop
+         else if(counter <=0){
             setIsActive(false)
-            setCounter(10)
          }
-         else if(isActive)
-         {
-            setIsActive(false)
-            setCounter(10);
-         }
-     }, [isActive, counter]);
+     }, [counter]);
     
   return (
     <>
