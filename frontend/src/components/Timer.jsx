e51926bc8211a5468af0c82f0e056638271da20d
Alan Jack <60137518+aldojack@users.noreply.github.com>
2023-02-18 22:43:24 +0000

    Timer component now handles the count

--- frontend/src/components/Timer.jsx
+++ frontend/src/components/Timer.jsx
@@ -1,24 +1,34 @@
 import { useState, useEffect } from 'react';
 
-export default function Timer({isActive, setIsActive, counter, setCounter}) {
+export default function Timer({isActive, setIsActive, initialCounter, resetTimer}) {
+  const [counter, setCounter] = useState(initialCounter);
 
-
-      useEffect(() => {
-         //If active and time left then countdown timer will decrement by 1 second
-         if(isActive && counter >0){
-            setTimeout(() => {
-                setCounter(prevCounter => prevCounter - 1)
-            }, 1000)
-         } 
-         //If counter is 0 then player is no longer active and movement should stop
-         else if(counter <=0){
-            setIsActive(false)
-         }
-     }, [counter]);
+  useEffect(() => {
+    let intervalId;
     
+    // If active and time left then start interval to decrement timer by 1 second
+    if (isActive && counter > 0) {
+      intervalId = setInterval(() => {
+        setCounter(prevCounter => prevCounter - 1);
+      }, 1000);
+    } 
+    // If counter is 0 then player is no longer active and movement should stop
+    else if (counter <= 0) {
+      setIsActive();
+    }
+
+    // Cleanup function to clear interval when component unmounts or dependency changes
+    return () => clearInterval(intervalId);
+  }, [isActive, counter, setIsActive]);
+
+  useEffect(() => {
+   if(resetTimer)
+   {
+      setCounter(initialCounter);
+   }
+ }, [resetTimer]);
+
   return (
-    <>
-        <div>{counter}</div>
-    </>
-  )
+    <div>{counter}</div>
+  );
 }
